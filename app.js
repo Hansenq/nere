be619e4b1c5b16eb1b8906f29aa6b156bbd0e5ed
Hansen Qian <Hansenq@gmail.com>
2013-01-06 01:31:35 -0800

    added a console log file to debut calculated distances

--- app.js
+++ app.js
@@ -149,6 +149,7 @@ function findNearestRoomLoc(latitude, longitude) {
       continue;
     }
     dist = calcDistance(latitude, longitude, room.cenLat, room.cenLong);
+    console.log('=============(' + latitude + ', ' + longitude + ') to (' + room.cenLat + ', ' + room.cenLong + ')===========' + dist)
     if (dist < room.radius) {
       if (dist < closestDist) {
         closestDist = dist;
