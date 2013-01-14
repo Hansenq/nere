1fab5b30baa3e53ce56457fed7e58540b1f3b3fd
Hansen Qian <Hansenq@gmail.com>
2013-01-14 00:40:48 -0500

    added google analytics

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -48,6 +48,18 @@ function changeRooms(roomId) {
   socket.roomId = roomId;
 }
 
+// Google Analytics
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-37624872-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 // Begin using socket.io
 var username = checkCookie('username');
 var datetime = (new Date()).getTime();
