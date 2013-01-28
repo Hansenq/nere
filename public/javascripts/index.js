8c38cec7a55afd887b27c9b394ba51efd0309cf6
Hansen Qian <Hansenq@gmail.com>
2013-01-28 08:52:23 -0500

    Revered changes in dealing with position

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -44,8 +44,16 @@ function message(chat, senderName) {
   $('.main').scrollTop($('.main').prop('scrollHeight'));
 }
 
-function trim(str) {
-
+// Sanitizes user input before adding it to DOM
+// NOTE: This is not a _____?
+function encodeHTML(s) {
+  return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+function decodeHTML(s) {
+  if (typeof s === "string")
+    return s.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+  else
+    return s;
 }
 
 // Begin using socket.io
@@ -185,7 +193,6 @@ socket.on('announcement', function (msg) {
 
 function dismissGSModal() {
   $('#gsModal').modal('hide');
-  //$('#gsModal .modal-footer .btn span').prop('value', 'Close');
 }
 
 // Configuring Get Started modal
@@ -198,29 +205,19 @@ $('#gsModal .modal-footer .btn').click(function() {
   }
 });
 
-// Sanitizes user input before adding it to DOM
-// NOTE: This is not a 
-function encodeHTML(s) {
-  return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-function decodeHTML(s) {
-  if (typeof s === "string")
-    return s.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
-  else
-    return s;
-}
-
 $(document).ready(function() {
 
   // Shows Get Started page
   $('#gsModal').modal('show');
 
   if (navigator.geolocation) {
+    $('#gsModal .modal-body').val('Please wait...');
     navigator.geolocation.getCurrentPosition(
       positionSuccess, 
       positionError, 
       {
-        enableHighAccuracy: true
+        enableHighAccuracy: true, 
+        timeout: 10000
       }
       );
   };
